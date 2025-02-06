03968aa398d111725cfd105a8ba06249bdad199d
Gabriel Lucero <nathaniel-lucero12@hotmail.com>
2025-02-05 20:39:58 -0500

    Primer Push

--- src/main/java/com/example/Mascotas/Controlador/MascotaController.java
+++ src/main/java/com/example/Mascotas/Controlador/MascotaController.java
@@ -1,11 +1,13 @@
 package com.example.Mascotas.Controlador;
 
+import com.example.Mascotas.Entidad.Mascota;
 import com.example.Mascotas.Servicio.MascotaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Optional;
 
 @Controller
 public class MascotaController {
@@ -16,5 +18,33 @@ public class MascotaController {
     public String listarMascotas(Model model) {
         model.addAttribute("mascotas", mascotaService.listarMascotas());
         return "mascotas";
+    }
+
+    // CREAR
+    @GetMapping("/formulario")
+    public String formularioMascota(Model model) {
+        model.addAttribute("mascota", new Mascota());
+        return "/Mascota/formulario";
+    }
+
+    @PostMapping("/guardar")
+    public String crearMascota(Mascota mascota) {
+        mascotaService.guardarMascota(mascota);
+        return "redirect:/mascotas";
+    }
+
+    // ACTUALIZAR
+    @GetMapping("/editar/{id}")
+    public String editarMascota(@PathVariable Long id, Model model) {
+        Optional<Mascota> mascota = mascotaService.buscarMascota(id);
+        model.addAttribute("mascota", mascota);
+        return "/Mascota/formulario";
+    }
+
+    // ELIMINAR
+    @GetMapping("/eliminar/{id}")
+    public String eliminarMascota(@PathVariable Long id) {
+        mascotaService.eliminarMascota(id);
+        return "redirect:/mascotas";
     }
 }
--- src/main/java/com/example/Mascotas/Servicio/MascotaService.java
+++ src/main/java/com/example/Mascotas/Servicio/MascotaService.java
@@ -4,7 +4,9 @@ import com.example.Mascotas.Entidad.Mascota;
 import com.example.Mascotas.Repositorio.MascotaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MascotaService {
@@ -13,5 +15,17 @@ public class MascotaService {
 
     public List<Mascota> listarMascotas() {
         return mascotaRepository.findAll();
+    }
+
+    public void guardarMascota(Mascota mascota) {
+        mascotaRepository.save(mascota);
+    }
+
+    public Optional<Mascota> buscarMascota(Long id) {
+        return mascotaRepository.findById(id);
+    }
+
+    public void eliminarMascota(Long id) {
+        mascotaRepository.deleteById(id);
     }
 }
